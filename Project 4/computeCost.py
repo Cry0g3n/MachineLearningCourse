91309781264bb0281232acfad66a442e7f6219f4
Andrew <andrey.shemiakov@gmail.com>
2017-04-18 00:05:50 +0300

    Практика №4

--- Project 4/computeCost.py
+++ Project 4/computeCost.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sigmoid import sigmoid
 
+
 def computeCost(X, y, num_labels, Theta1, Theta2, lam):
     """
         Функция позволяет вычислить значение стоимостной функции, 
@@ -8,7 +9,7 @@ def computeCost(X, y, num_labels, Theta1, Theta2, lam):
         для нейронной сети, параметр регуляризации lam, матрицу 
         объекты-признаки X и вектор меток y
     """
-    
+
     m = y.shape[0]
     J = 0
 
@@ -16,9 +17,27 @@ def computeCost(X, y, num_labels, Theta1, Theta2, lam):
     # Инструкция: вычислить значение стоимостной функции для заданных 
     # Theta1, Theta2, lam, X и y. Присвоить полученный результат в J 
     # для стоимостной функции
-    
-    
-    
+
+    a1 = X
+    a2 = sigmoid(np.dot(a1, Theta1.transpose()))
+    a2 = np.concatenate((np.ones((m, 1)), a2), axis=1)
+
+    a3 = sigmoid(np.dot(a2, Theta2.transpose()))
+    h = a3
+
+    Y = np.zeros((m, num_labels))
+    for c in range(num_labels):
+        Y[np.where(y == c)[0], c] = 1
+
+    for i in range(m):
+        for k in range(num_labels):
+            J = J + (-Y[i, k] * np.log(h[i, k]) - (1 - Y[i, k]) * np.log(1 - h[i, k]))
+
+    J = J / m
+
+    J = J + lam / (2 * m) * (
+    np.sum(Theta1 ** 2) + np.sum(Theta2 ** 2) - np.sum(Theta1[:, 0] ** 2) - np.sum(Theta2[:, 0] ** 2))
+
     # ============================================================
-    
+
     return J
--- Project 4/gradientDescent.py
+++ Project 4/gradientDescent.py
@@ -3,6 +3,7 @@ from computeCost import computeCost
 from sigmoid import sigmoid
 from sigmoidGradient import sigmoidGradient
 
+
 def gradientDescent(X, y, num_labels, Theta1, Theta2, alpha, num_iters, lam):
     """
         Функция позволяет выполнить градиентный спуск для поиска 
@@ -11,7 +12,7 @@ def gradientDescent(X, y, num_labels, Theta1, Theta2, alpha, num_iters, lam):
         параметр сходимости alpha, число итераций алгоритма num_iters 
         и параметр регуляризации lam
     """
-    
+
     J_history = []
     m = y.shape[0]
 
@@ -21,24 +22,41 @@ def gradientDescent(X, y, num_labels, Theta1, Theta2, alpha, num_iters, lam):
     Y = np.zeros((m, num_labels))
     for c in range(num_labels):
         Y[np.where(y == c)[0], c] = 1
-    
-    for i in range(num_iters):
 
+    for i in range(num_iters):
         print('Эпоха обучения №', i + 1)
-        
+
         # ====================== Ваш код здесь ======================
-        # Инструкция: выполнить алгоритм обратного распространения ошибки 
-        # с целью поиска частных производных от стоимостной функции по 
+        # Инструкция: выполнить алгоритм обратного распространения ошибки
+        # с целью поиска частных производных от стоимостной функции по
         # параметрам модели
-        
-        
-        
+        A1 = X
+        Z1 = sigmoid(np.dot(A1, Theta1.T))
+        A2 = np.concatenate((np.ones((Z1.shape[0], 1)), Z1), axis=1)
+        Z2 = sigmoid(np.dot(A2, Theta2.T))
+        H = Z2
+
+        Y = (np.multiply(np.ones(H.shape), np.arange(H.shape[1])) == y).astype('int')
+
+        D3 = (H - Y).T
+        D2 = np.dot(Theta2.T, D3)[1:, :] * sigmoidGradient(np.dot(A1, Theta1.T)).T
+
+        Delta1 = np.dot(D2, A1)
+        Delta2 = np.dot(D3, A2)
+
+        Temp1 = np.copy(Theta1)
+        Temp1[:, 0] = 0
+        Temp2 = np.copy(Theta2)
+        Temp2[:, 0] = 0
+
+        Theta1_grad = Delta1 / m + Temp1 * lam / m
+        Theta2_grad = Delta2 / m + Temp2 * lam / m
         # ============================================================
-        
+
         Theta1 = Theta1 - alpha * Theta1_grad
         Theta2 = Theta2 - alpha * Theta2_grad
-        
-        J_history.append(computeCost(X, y, num_labels, Theta1, Theta2, lam)) # сохранение значений стоимостной функции
-                                                                             # на каждой итерации
-    
+
+        J_history.append(computeCost(X, y, num_labels, Theta1, Theta2, lam))  # сохранение значений стоимостной функции
+        # на каждой итерации
+
     return Theta1, Theta2, J_history
