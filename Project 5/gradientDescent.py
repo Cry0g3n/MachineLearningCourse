e60093c7df5b5878548ef5374673a2a98a7fb798
Andrew <andrey.shemiakov@gmail.com>
2017-04-24 22:32:01 +0300

    Практика №5

---
+++ Project 5/gradientDescent.py
@@ -0,0 +1,31 @@
+import numpy as np
+from computeCost import computeCost
+
+def gradientDescent(X, y, theta, alpha, num_iters, lam):
+    """
+        Функция позволяет выполнить градиентный спуск для поиска 
+        параметров модели theta, используя матрицу объекты-признаки X, 
+        вектор меток y, параметр сходимости alpha и число итераций 
+        алгоритма num_iters
+    """
+    
+    J_history = []
+    m = y.shape[0]
+
+    J_history.append(computeCost(X, y, theta, lam))
+
+    for i in range(num_iters):
+        
+        # ====================== Ваш код здесь ======================
+        # Инструкция: выполнить градиентный спуск для num_iters итераций 
+        # с целью вычисления вектора параметров theta, минимизирующего 
+        # стоимостную функцию
+        
+        
+        
+        # ============================================================
+        
+        J_history.append(computeCost(X, y, theta, lam)) # сохранение значений стоимостной функции
+                                                        # на каждой итерации
+    
+    return theta, J_history
