419beef27df53c3dd6a67131b58188a37755380b
Andrew <andrey.shemiakov@gmail.com>
2017-05-06 23:18:38 +0300

    Практика №7

--- Project 7/main.py
+++ Project 7/main.py
@@ -1,16 +1,15 @@
 ## Практическое задание № 7. Анализ главных компонент
 
-# Инициализация
-import numpy as np
-import scipy.io as spi
 import matplotlib.pyplot as plt
-from numpy.matlib import repmat
 
+# Инициализация
+import scipy.io as spi
+from displayData import displayData
 from featureNormalize import featureNormalize
+from numpy.matlib import repmat
 from pca import pca
 from projectData import projectData
 from recoverData import recoverData
-from displayData import displayData
 
 # ================= Часть 1. Визуализация данных =================
 
@@ -28,7 +27,7 @@ plt.ylabel('Второй признак')
 plt.grid()
 plt.show()
 
-input('Программа остановлена. Нажмите Enter для продолжения ... \n')
+# input('Программа остановлена. Нажмите Enter для продолжения ... \n')
 
 # ============== Часть 2. Анализ главных компонент ===============
 
@@ -43,7 +42,7 @@ U, S = pca(X_norm)
 print('Матрица собственных векторов:')
 print(U)
 
-input('Программа остановлена. Нажмите Enter для продолжения ... \n')
+# input('Программа остановлена. Нажмите Enter для продолжения ... \n')
 
 # =============== Часть 3. Сокращение размерности ================
 
@@ -60,15 +59,15 @@ print('Первые три восстановленных примера:')
 print(X_rec[0:3, :])
 
 # Визуализация данных
-plt.plot(X_norm[:, 0], X_norm[:, 1], 'bo', label = 'Исходные данные')
-plt.plot(X_rec[:, 0],  X_rec[:, 1],  'rx', label = 'Аппроксимация данных')
-plt.legend(loc = 'upper right', shadow = True, fontsize = 12, numpoints = 1)
+plt.plot(X_norm[:, 0], X_norm[:, 1], 'bo', label='Исходные данные')
+plt.plot(X_rec[:, 0], X_rec[:, 1], 'rx', label='Аппроксимация данных')
+plt.legend(loc='upper right', shadow=True, fontsize=12, numpoints=1)
 plt.xlabel('Первый признак')
 plt.ylabel('Второй признак')
 plt.grid()
 plt.show()
 
-input('Программа остановлена. Нажмите Enter для продолжения ... \n')
+# input('Программа остановлена. Нажмите Enter для продолжения ... \n')
 
 # ========== Часть 4. Загрузка и визуализация базы лиц ===========
 
@@ -91,14 +90,14 @@ U, S = pca(X_norm)
 # Визуализация собственных лиц
 displayData(U[:, 0:36].transpose(), 6)
 
-input('Программа остановлена. Нажмите Enter для продолжения ... \n')
+# input('Программа остановлена. Нажмите Enter для продолжения ... \n')
 
 # ======= Часть 5. Сокращение размерности изображений лиц ========
 
 print('Часть 5. Сокращение размерности изображений лиц')
 
 # Задание числа числа сохраненных главных компонент
-K = 100
+K = 70
 
 # Выполнение сокращения размерности данных
 Z = projectData(X_norm, U, K)
